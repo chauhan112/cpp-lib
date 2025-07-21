0e0e93645cc9871c75f62f46d25ddd248c7a67ab
raja <rajababuchauhan500@gmail.com>
2025-07-21 23:49:54 +0200

    hello world running explanation

--- helloWorld/hello.cpp
+++ helloWorld/hello.cpp
@@ -1,7 +1,11 @@
 #include <iostream> 
 #include "py_builtins.hpp"
 
-// 
+// first of all compile each file into object files
+// g++ -c helloWorld/hello.cpp
+// g++ -c helloWorld/py_builtins.hpp
+// then link the object files together
+// g++ helloWorld/hello.o helloWorld/py_builtins.o -o helloWorld/hello
 int main() {
     print();
     return 0;
